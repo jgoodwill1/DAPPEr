ed68b398775c074d166b15d0a6f19141614344d7
jgoodwill1 <goodwill@udel.edu>
2024-04-12 08:52:17 -0400

    sweep fits

--- UDIP_Fitting.py
+++ UDIP_Fitting.py
@@ -7,11 +7,15 @@ Created on March 25, 2023
 import numpy as np
 from scipy.optimize import curve_fit
 
+import numpy as np
+from scipy.optimize import curve_fit
+
 '''
 def gen_fit(x, y):
 Generates fit of linear and sqrt portion of IV arrays. Refer to
 data_processing to idenify filtering of IV arrays. scipy.optimize.curve_fit
 used to fit non-linear least squares to fit
+
 
 input:
 V_arr (array); Voltage array
@@ -23,13 +27,23 @@ V_fit (np.array); linear space between min and max voltages from V_arr
 model(t, *popt) (np.array); fitted model of I_arr using Voltage range and 
                             pointer to popt
 popt(array); optimal values for parameters
-  V_f  (float) = popt[0]; Plasma potential
+  V_f  (float) = popt[0]; floating potential
   m1   (float) = popt[1]; slope of linear fit
-  b    (float) = popt[2]  (float); y-intercept of linear fit
-  ne   (float) = popt[3]; electron density [cm^{-3}]
-  etemp(float) = popt[4]; electron temperature [K]
-  VP   (float) = popt[5]; negative plasma potential [V]
+  b    (float) = popt[2]; y-intercept of linear fit
+  a    (float) = popt[3]; multiplication factor of exponential fit
+  ne   (float) = popt[4]; electron density [cm^{-3}]
+  etemp(float) = popt[5]; electron temperature [K]
+  VP   (float) = popt[6]; plasma potential [V]
   
+Intial guesses/[bounds]:
+    V_f   = 0.3   ; [0: 0.5]
+    m1    = 100   ; [-1000:1000]
+    b     = 80    ; [-100:100]
+    a     = 5     ; [0:100]
+    ne    = 8*10^8; [0, np.inf]
+    etemp = 1000  ; [0, 10000]
+    VP    = 0.9   ; [0.5, 1.5]
+
 pcov(2D np.array); covariance of popt array
 '''
 
@@ -40,10 +54,15 @@ def gen_fit(V_arr, I_arr, proc = False):
         V_proc, I_proc = data_processing(V_arr, I_arr)
     else:
         V_proc, I_proc = V_arr, I_arr
-    guess = [0.6, 100, 80, 8*(10**10), 1000,-0.5]    #intial guess
-    b = ((0,-1000,-100,0,0,-3),(3,1000,100,np.inf,10000,3)) #bounds
+    guess = [0.3, 100, 80,  5,  8*(10**10), 1000, 0.9]    #intial guess
+    b = ((0,-1000,-100, 0, 0, 0, 0.5),(0.5,1000,100, 100, np.inf,10000,1.5)) #bounds
+
+    # guess = [0.5, 100, 80, 1000, 0.5, 1]    #intial guess
+    # b = ((0, -1000, -100, 0, 0, 0.5),(1, 1000, 100, 10000, 100, 3)) #bounds
+
     popt, pcov = curve_fit(model, V_proc, I_proc, guess, bounds = b)
-    V_fit = np.linspace(min(V_proc),max(V_proc), num = 200) #Voltage array processed for fit
+    # popt, pcov = curve_fit(model, V_proc, I_proc)
+    V_fit = np.linspace(min(V_proc),max(V_proc), num = 500) #Voltage array processed for fit
     return V_fit, model(V_fit,*popt), popt, pcov
 
 '''
@@ -57,20 +76,29 @@ m1 (float); slope of linear fit
 b  (float); y-intercept of linear fit
 ne (float); electron density [cm^{-2}]
 etemp(float); electron temperature [K]
-VP (float); negative plasma potential [V]
+VP (float); plasma potential [V]
 '''
 
-def model(V_proc, Vf, m1, b, ne, etemp, VP):
+def model(V_proc, Vf, m1, b, a, ne, etemp, VP):
     I_fit = np.zeros(len(V_proc))
     #Linear fit for ion saturation
-    I_fit[V_proc <= Vf] = lin_fit(V_proc[V_proc <= Vf], m1, b) 
+    I_fit[V_proc <= Vf] = lin_fit(V_proc[V_proc <= Vf], m1, b)
+    I1_cont =  -(exp_fit(Vf, a, etemp, Vf) + lin_fit(Vf, m1, b))
+    #Exp fit for transition region
+    I_fit[(V_proc > Vf) & (V_proc <= VP)] = exp_fit(V_proc[(V_proc > Vf) & (V_proc <= VP)], a, etemp,  Vf) + I1_cont
+    I2_cont = -(exp_fit(VP, a, etemp,  Vf) + I1_cont)
     #Sqrt fit for electron saturation
-    I_fit[V_proc > Vf] = sqrt_fit(V_proc[V_proc > Vf], ne, etemp ,VP) \
-                         - sqrt_fit(Vf, ne , etemp, VP)
+    I_fit[V_proc > VP] = sqrt_fit(V_proc[V_proc > VP], ne, etemp ,VP) + I2_cont
     return I_fit
 
 def lin_fit(x, m, y_int): #linear--full model square root
     return m * x + y_int
+
+def exp_fit(x, a, etemp, Vf): #exponential fit
+    q_e = 1.602 * 10**-19 #electron charge [C]
+    K_b = 1.381 * 10**-23 #boltzman constant [m^2*kg/(s^2*K)]   
+    k = q_e / (K_b * etemp)
+    return a * np.exp(k * (x - Vf))
 
 '''
 def sqrt_fit(x, ne, etemp, V0)
@@ -95,7 +123,7 @@ def sqrt_fit(x, ne, etemp, VP):# square root
 
     k = q_e / (K_b * etemp)
     I0 = ne * q_e * np.sqrt(K_b * etemp / (2. * np.pi * m_e)) * A / (10**-9)
-    return I0 * np.sqrt(1. + k*(x + VP))
+    return I0 * np.sqrt(1. + k*(x - VP))
 
 '''
 def data_processing(V, I)
